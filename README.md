82f4fbeeae31373585d62cc9d7c1fcf048fc91d7
Guanghui Wang <guanghui-wang@foxmail.com>
2017-11-26 16:32:01 +0800

    initial vue by vue-cli

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-# React h5 demo
+# h5-vue-project-review
 
-## Running Project
+> A Vue.js project
 
-```bash
-yarn
-npm start
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
 ```
 
-Username: test, Password: test
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- webpack.config.js
+++ webpack.config.js
@@ -1,167 +1,108 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // create index.html injecting index_bundle.js in dist folder
-const webpack = require('webpack');
+var path = require('path')
+var webpack = require('webpack')
 
-const config = {
-  resolve: {
-    extensions: ['*', '.js', '.jsx', '.json']
-  },
-  devtool: 'cheap-module-eval-source-map', // more info:https://webpack.js.org/guides/development/#using-source-maps and https://webpack.js.org/configuration/devtool/
-  entry: {
-    // must be first entry to properly set public path
-    // './src/webpack-public-path',
-    // 'react-hot-loader/patch',
-    // 'webpack-hot-middleware/client?reload=true',
-    // babel-polyfill: Uncaught ReferenceError: regeneratorRuntime is not defined
-    vendor: ['babel-polyfill', 'react', 'react-dom', 'prop-types', 'axios'],
-    app: [path.resolve(__dirname, 'src/index.js')]
-    // Defining path seems necessary for this to work consistently on Windows machines.
-  },
-  target: 'web',
+module.exports = {
+  entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'), // Note: Physical files are only output by the production build task `npm run build`.
-    publicPath: '/',
-    filename: '[name].js'
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
   },
-  devServer: {
-    historyApiFallback: true
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'), // Tells React to build in either dev or prod modes. https://facebook.github.io/react/downloads.html (See bottom)
-      __DEV__: true
-    }),
-    // new webpack.HotModuleReplacementPlugin(),
-    // new webpack.NoEmitOnErrorsPlugin(),
-    new HtmlWebpackPlugin({
-      // Create HTML file that includes references to bundled CSS and JS.
-      template: 'src/index.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true
-      },
-      inject: true
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
-    })
-  ],
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.eot(\?v=\d+.\d+.\d+)?$/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        test: /\.css$/,
         use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10000,
-              mimetype: 'application/font-woff'
-            }
-          }
-        ]
+          'vue-style-loader',
+          'css-loader'
+        ],
       },
       {
-        test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
+        test: /\.scss$/,
         use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10000,
-              mimetype: 'application/octet-stream'
-            }
-          }
-        ]
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
       },
       {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.sass$/,
         use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10000,
-              mimetype: 'image/svg+xml'
-            }
-          }
-        ]
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader?indentedSyntax'
+        ],
       },
       {
-        test: /\.(jpe?g|png|gif|ico)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'assets/img/[name].[ext]'
-            }
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this necessary.
+            'scss': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader'
+            ],
+            'sass': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader?indentedSyntax'
+            ]
           }
-        ]
+          // other vue-loader options go here
+        }
       },
       {
-        test: /\.(mp3)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'assets/[name].[ext]'
-            }
-          }
-        ]
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
       },
       {
-        test: /(\.css|\.scss|\.sass)$/,
-        exclude: /node_modules/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-              modules: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]'
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: () => [require('autoprefixer')],
-              sourceMap: true
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              includePaths: [path.resolve(__dirname, 'src', 'scss')],
-              sourceMap: true,
-              modules: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]'
-            }
-          }
-        ]
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
       }
     ]
-  }
-};
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
 
-/*// 1. package.json npm run build will set node env production.
-// 2. NODE_ENV in DefinePlugin: webpack will build this into bundle.js so React realizes it's for production now
 if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        NODE_ENV: '"production"'
       }
     }),
-    new webpack.optimize.UglifyJsPlugin()
-  );
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
 }
-*/
-
-module.exports = config;
