86cc38df57eb979b70111881675e1fc5b7e990b3
Guanghui Wang <guanghui-wang@foxmail.com>
2017-11-30 17:21:46 +0800

    增加新的 http allow origin

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', true);
   next();
